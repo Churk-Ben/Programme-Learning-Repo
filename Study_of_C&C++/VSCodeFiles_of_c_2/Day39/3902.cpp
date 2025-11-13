b703971d95ae75c3b5bb97fa76e69ff9b81b48a3
churk_ben <2779675416@qq.com>
2025-11-13 23:05:59 +0800

    refactor: 移除不必要的栈头文件和实例化，优化代码结构

--- Study_of_C&C++/VSCodeFiles_of_c_2/Day39/3902.cpp
+++ Study_of_C&C++/VSCodeFiles_of_c_2/Day39/3902.cpp
@@ -1,11 +1,6 @@
 #include <iostream>
 #include <string>
 #include <windows.h>
-
-#include <stack>
-
-stack<int> st;
-
 using namespace std;
 
 string MAP[16] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
